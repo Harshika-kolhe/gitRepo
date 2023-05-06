182c89dc7fb8807f906cede6bc858ebd78baa4f2
Harshika1999 <kolheharshika@gmail.com>
2023-05-06 23:11:48 +0530

    newindex add

---
+++ demo.js
@@ -0,0 +1,13 @@
+// console.log(document);
+console.log(document.title);
+document.title="hello";
+
+
+var header= document.getElementById('header-title');
+console.log(header);
+header.style.border='solid 3px #000';
+
+var addItems=document.querySelector('h2');
+console.log(addItems);
+addItems.style.backgroundColor='green';
+addItems.style.fontWeight='bold';
