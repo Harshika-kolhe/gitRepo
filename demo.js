3f9958cbb3ae9cf21834356900671840d9ddb196
Harshika1999 <kolheharshika@gmail.com>
2023-05-16 23:34:14 +0530

    Modifying DOM

--- demo.js
+++ demo.js
@@ -27,23 +27,77 @@
 
 // }
 
-var secondItem=document.querySelector('.list-group-item:nth-child(2)');
-//console.log(secondItem);
-secondItem.style.backgroundColor='green';
+// var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+// //console.log(secondItem);
+// secondItem.style.backgroundColor='green';
 
-var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
-//console.log(thirdItem);
-thirdItem.style.visibility='hidden';
+// var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
+// //console.log(thirdItem);
+// thirdItem.style.visibility='hidden';
 
-var list=document.querySelectorAll('.list-group-item');
-//console.log(list);
+// var list=document.querySelectorAll('.list-group-item');
+// //console.log(list);
 
-list[1].style.color='green';
+// list[1].style.color='green';
 
-var listOdd=document.querySelectorAll('.list-group-item:nth-child(Odd)');
+// var listOdd=document.querySelectorAll('.list-group-item:nth-child(Odd)');
 
-for(var i=0;i<listOdd.length;i++){
-    console.log(listOdd[i]);
+// for(var i=0;i<listOdd.length;i++){
+//     console.log(listOdd[i]);
 
-    listOdd[i].style.backgroundColor='green';
-}
+//     listOdd[i].style.backgroundColor='green';
+// }
+
+// parentElement
+
+var list = document.querySelector('#items');
+console.log(list);
+
+console.log(list.parentElement);
+console.log(list.parentNode);
+
+// lastelementchild
+console.log(list.lastElementChild);
+// lastchild
+console.log(list.lastChild);
+// createchild
+
+// firstelementchild
+console.log(list.firstElementChild);
+
+// firstchild
+console.log(list.firstChild);
+
+// nextsibling
+console.log(list.nextSibling);
+
+// nextelementsibling
+console.log(list.nextElementSibling);
+
+// previoussibling
+console.log(list.previousSibling);
+
+// previouselementsibling
+console.log(list.previousElementSibling);
+list.previousElementSibling.style.color='red';
+// createelement
+var divEle=document.createElement('div');
+var divText=document.createTextNode('HEllo');
+//divEle.id='hello1';
+divEle.appendChild(divText);
+var container=document.querySelector('#main');
+console.log(container);
+
+var itemlist=document.querySelector('.title');
+console.log(itemlist);
+var item1=document.querySelector('#items');
+//add before list
+container.insertBefore(divEle,itemlist);
+container.insertBefore(divEle,item1);
+//add before item 1
+
+
+
+// setAttribute
+// createtesxtnode
+// appendchild
